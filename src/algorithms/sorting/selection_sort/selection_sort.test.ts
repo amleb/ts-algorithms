6b76b132d0148769d505fca1ba496e714dce25ad
Adam Lebioda <adam.m.lebioda@gmail.com>
2023-04-12 14:27:37 +0200

    add prettier

--- src/algorithms/sorting/selection_sort/selection_sort.test.ts
+++ src/algorithms/sorting/selection_sort/selection_sort.test.ts
@@ -1,6 +1,12 @@
 import selectionSort from "./selection_sort";
 
-test('selection sort', () => {
-    expect(selectionSort([5, 3, 6, 1, 2, 4])).toEqual([1, 2, 3, 4, 5, 6]);
-    expect(selectionSort(['d', 'a', 'c', 'b', 'e'])).toEqual(['a', 'b', 'c', 'd', 'e']);
+test("selection sort", () => {
+  expect(selectionSort([5, 3, 6, 1, 2, 4])).toEqual([1, 2, 3, 4, 5, 6]);
+  expect(selectionSort(["d", "a", "c", "b", "e"])).toEqual([
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+  ]);
 });
--- src/algorithms/sorting/selection_sort/selection_sort.ts
+++ src/algorithms/sorting/selection_sort/selection_sort.ts
@@ -1,20 +1,20 @@
 function selectionSort<T>(arr: T[]): T[] {
-    const length = arr.length;
+  const length = arr.length;
 
-    for (let i = 0; i < length - 1; i++) {
-        let minIdx = i;
-        for (let j = i + 1; j < length; j++) {
-            if (arr[j] < arr[minIdx]) {
-                minIdx = j;
-            }
-        }
-
-        if (i !== minIdx) {
-            [arr[minIdx], arr[i]] = [arr[i], arr[minIdx]];
-        }
+  for (let i = 0; i < length - 1; i++) {
+    let minIdx = i;
+    for (let j = i + 1; j < length; j++) {
+      if (arr[j] < arr[minIdx]) {
+        minIdx = j;
+      }
     }
 
-    return arr;
+    if (i !== minIdx) {
+      [arr[minIdx], arr[i]] = [arr[i], arr[minIdx]];
+    }
+  }
+
+  return arr;
 }
 
 export default selectionSort;
