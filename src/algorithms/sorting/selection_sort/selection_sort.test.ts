0da7c347b7bc8648fb4cddc4ceb7508aaf9eb0b2
Adam Lebioda <adam.m.lebioda@gmail.com>
2023-04-14 15:47:30 +0200

    reformat test cases

--- src/algorithms/sorting/selection_sort/selection_sort.test.ts
+++ src/algorithms/sorting/selection_sort/selection_sort.test.ts
@@ -1,13 +1,18 @@
 import selectionSort from "./selection_sort";
 import doubleSelectionSort from "./double_selection_sort";
-import { sortingTestCases } from  "../test_cases"
+import sortingTestCases from "../test_cases";
 
-describe('selection sort', () => {
-  test.each(sortingTestCases)("selection sort", (testCase) => {
+describe("selection sort", () => {
+  test.each(sortingTestCases)("selection sort: $testType", (testCase) => {
     expect(selectionSort([...testCase.unsorted])).toEqual(testCase.sorted);
   });
 
-  test.each(sortingTestCases)("double selection sort", (testCase) => {
-    expect(doubleSelectionSort([...testCase.unsorted])).toEqual(testCase.sorted);
-  });
+  test.each(sortingTestCases)(
+    "double selection sort: $testType",
+    (testCase) => {
+      expect(doubleSelectionSort([...testCase.unsorted])).toEqual(
+        testCase.sorted
+      );
+    }
+  );
 });
--- src/algorithms/sorting/test_cases.ts
+++ src/algorithms/sorting/test_cases.ts
@@ -1,16 +1,54 @@
 // type SortingTestCase = [number[], number[]];
-export const sortingTestCases = [
-  { unsorted: [], sorted: [] },
-  { unsorted: [3], sorted: [3] },
-  { unsorted: [1, 2], sorted: [1, 2] },
-  { unsorted: [2, 1], sorted: [1, 2] },
-  { unsorted: [1, 2, 3, 4, 5, 6], sorted: [1, 2, 3, 4, 5, 6] },
-  { unsorted: [6, 5, 4, 3, 2, 1], sorted: [1, 2, 3, 4, 5, 6] },
-  { unsorted: [5, 3, 6, 1, 2, 4], sorted: [1, 2, 3, 4, 5, 6] },
-  { unsorted: [1, 1, 1, 2, 4], sorted: [1, 1, 1, 2, 4] },
+const sortingTestCases = [
+  { unsorted: [], sorted: [], testType: "empty array" },
+  { unsorted: [3], sorted: [3], testType: "an array with one element" },
+  { unsorted: [1, 2], sorted: [1, 2], testType: "two sorted elements" },
+  {
+    unsorted: [2, 1],
+    sorted: [1, 2],
+    testType: "two elements in reversed order",
+  },
+  {
+    unsorted: [1, 2, 3, 4, 5, 6],
+    sorted: [1, 2, 3, 4, 5, 6],
+    testType: "sorted array",
+  },
+  {
+    unsorted: [6, 5, 4, 3, 2, 1],
+    sorted: [1, 2, 3, 4, 5, 6],
+    testType: "reversed array",
+  },
+  {
+    unsorted: [5, 3, 6, 1, 2, 4],
+    sorted: [1, 2, 3, 4, 5, 6],
+    testType: "unsorted array with even number of elements",
+  },
+  {
+    unsorted: [5, 3, 6, 1, 2, 7, 4],
+    sorted: [1, 2, 3, 4, 5, 6, 7],
+    testType: "unsorted array with odd number of elements",
+  },
+  {
+    unsorted: [1, 1, 1, 2, 2, 4, 4],
+    sorted: [1, 1, 1, 2, 2, 4, 4],
+    testType: "an array with duplicates",
+  },
   {
     unsorted: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     sorted: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    testType: "an array with identical elements",
   },
-  { unsorted: [-5, 3, -6, 1, 2, 4], sorted: [-6, -5, 1, 2, 3, 4] },
+  {
+    unsorted: [-5, 3, -6, 1, 2, 4],
+    sorted: [-6, -5, 1, 2, 3, 4],
+    testType: "an array with negative values",
+  },
 ];
+
+//tests should not mutate test cases
+sortingTestCases.forEach(({ sorted, unsorted }) => {
+  Object.freeze(sorted);
+  Object.freeze(unsorted);
+});
+
+export default sortingTestCases;
