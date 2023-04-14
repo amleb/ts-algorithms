672979203137bf7868b0b8d055420fdadaaa8d93
Adam Lebioda <adam.m.lebioda@gmail.com>
2023-04-14 15:58:10 +0200

    adjust prettier settings

--- src/algorithms/sorting/selection_sort/selection_sort.test.ts
+++ src/algorithms/sorting/selection_sort/selection_sort.test.ts
@@ -1,18 +1,18 @@
-import selectionSort from "./selection_sort";
-import doubleSelectionSort from "./double_selection_sort";
-import sortingTestCases from "../test_cases";
+import selectionSort from './selection_sort';
+import doubleSelectionSort from './double_selection_sort';
+import sortingTestCases from '../test_cases';
 
-describe("selection sort", () => {
-  test.each(sortingTestCases)("selection sort: $testType", (testCase) => {
+describe('selection sort', () => {
+  test.each(sortingTestCases)('selection sort: $testType', (testCase) => {
     expect(selectionSort([...testCase.unsorted])).toEqual(testCase.sorted);
   });
 
   test.each(sortingTestCases)(
-    "double selection sort: $testType",
+    'double selection sort: $testType',
     (testCase) => {
       expect(doubleSelectionSort([...testCase.unsorted])).toEqual(
-        testCase.sorted
+        testCase.sorted,
       );
-    }
+    },
   );
 });
--- src/algorithms/sorting/test_cases.ts
+++ src/algorithms/sorting/test_cases.ts
@@ -1,47 +1,47 @@
 // type SortingTestCase = [number[], number[]];
 const sortingTestCases = [
-  { unsorted: [], sorted: [], testType: "empty array" },
-  { unsorted: [3], sorted: [3], testType: "an array with one element" },
-  { unsorted: [1, 2], sorted: [1, 2], testType: "two sorted elements" },
+  { unsorted: [], sorted: [], testType: 'empty array' },
+  { unsorted: [3], sorted: [3], testType: 'an array with one element' },
+  { unsorted: [1, 2], sorted: [1, 2], testType: 'two sorted elements' },
   {
     unsorted: [2, 1],
     sorted: [1, 2],
-    testType: "two elements in reversed order",
+    testType: 'two elements in reversed order',
   },
   {
     unsorted: [1, 2, 3, 4, 5, 6],
     sorted: [1, 2, 3, 4, 5, 6],
-    testType: "sorted array",
+    testType: 'sorted array',
   },
   {
     unsorted: [6, 5, 4, 3, 2, 1],
     sorted: [1, 2, 3, 4, 5, 6],
-    testType: "reversed array",
+    testType: 'reversed array',
   },
   {
     unsorted: [5, 3, 6, 1, 2, 4],
     sorted: [1, 2, 3, 4, 5, 6],
-    testType: "unsorted array with even number of elements",
+    testType: 'unsorted array with even number of elements',
   },
   {
     unsorted: [5, 3, 6, 1, 2, 7, 4],
     sorted: [1, 2, 3, 4, 5, 6, 7],
-    testType: "unsorted array with odd number of elements",
+    testType: 'unsorted array with odd number of elements',
   },
   {
     unsorted: [1, 1, 1, 2, 2, 4, 4],
     sorted: [1, 1, 1, 2, 2, 4, 4],
-    testType: "an array with duplicates",
+    testType: 'an array with duplicates',
   },
   {
     unsorted: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     sorted: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    testType: "an array with identical elements",
+    testType: 'an array with identical elements',
   },
   {
     unsorted: [-5, 3, -6, 1, 2, 4],
     sorted: [-6, -5, 1, 2, 3, 4],
-    testType: "an array with negative values",
+    testType: 'an array with negative values',
   },
 ];
 
